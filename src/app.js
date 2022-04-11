56ebc913b354ae7374dfd9ace4a212887ea002de
Kevin X <tnkgavas363@gmail.com>
2022-04-11 15:10:56 -0700

    Added basic functions to open a text area for a note to be written in. Additionally, added an initialization function to init page.

--- src/app.js
+++ src/app.js
@@ -5,3 +5,36 @@ const notes = [
     id: 1 
   }
 ]
+
+const baseTemp = `
+<textarea id = 'textBox' rows="50" cols="85" placeholder = "Enter Note..."></textarea>
+<button class="Save">Save</button>
+<button class="Delete">Delete</button>
+`
+
+function emptyNote(template){
+  const noteDisplayArea = document.querySelector('.write-note-area')
+  noteDisplayArea.innerHTML = template                                          
+}
+
+function clearWrite(){
+  const writeArea = document.querySelector('.write-note-area')
+  writeArea.innerHTML = ''
+}
+
+function createNote (){
+  emptyNote(baseTemp)
+  delBtn = document.querySelector('.Delete')
+  delBtn.addEventListener('click', clearWrite)
+}
+
+function enableAddButton(){
+  const addButton = document.querySelector('.icons')
+  addButton.addEventListener('click', createNote)
+}
+
+function initPage(){
+  enableAddButton()
+}
+
+initPage()
